4f67696974c52809d6b15ce5d82dc46ed192bca1
Vivid Platform <35667543+vividplatform@users.noreply.github.com>
2018-07-10 16:30:03 +0200

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -51,7 +51,7 @@ static const int MAX_URI_LENGTH = 255;
 #define SPINNER_FRAMES 35
 
 #define QAPP_ORG_NAME "Vivid"
-#define QAPP_ORG_DOMAIN "savebitcoin.io"
+#define QAPP_ORG_DOMAIN "vividcoin.app"
 #define QAPP_APP_NAME_DEFAULT "Vivid-Qt"
 #define QAPP_APP_NAME_TESTNET "Vivid-Qt-testnet"
 
